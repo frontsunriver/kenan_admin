d35d2feff974626074601b722fcb22e53bd81fc2
Sunriver <seriousman217winter@gmail.com>
2024-12-05 16:23:18 +0900

    feat(:update)Update api connections router

--- config/constant.js
+++ config/constant.js
@@ -1,6 +1,6 @@
 const product_mode = 1; // 1: production, 0: development
 
-export const SERVER_URL = product_mode == 0 ? "http://192.168.144.241:8000/api" : "https://10.6.0.14:443/api";
-export const DOWNLOAD_URL = product_mode == 0 ? "http://192.168.144.241:8000/" :"https://10.6.0.14:443/"
+export const SERVER_URL = product_mode == 0 ? "http://192.168.144.241:8000/api" : "https://10.6.0.16:8443/api";
+export const DOWNLOAD_URL = product_mode == 0 ? "http://192.168.144.241:8000/" :"https://10.6.0.16:8443/"
 
 export const USER_INFO = "user_info";
